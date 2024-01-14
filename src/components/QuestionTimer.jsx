c8ddc93576f79945870149b5c2816a92e881f048
Ravi <lcs2020043@iiitl.ac.in>
2024-01-15 00:47:07 +0530

    QuestionTimer

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 export default function QuestionTimer({ timeout, onTimeout }) {
-  setTimeout(onTimeout, timeout);
-  return <progress id="question-time" value="0" max="100"></progress>;
+  const [remainingTime, setRemainingTime] = useState(timeout);
+  useEffect(() => {
+    setTimeout(onTimeout, timeout);
+  }, [timeout, onTimeout]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((remainingTime) => remainingTime - 100);
+    }, 100);
+  }, []);
+  return (
+    <progress id="question-time" value={remainingTime} max={timeout}></progress>
+  );
 }
